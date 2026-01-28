578e538d0a4560ac231d87f537c7ecdeceb4ff68
srxshiv <srxshiv@gmail.com>
2026-01-28 16:37:17 +0530

    Added landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import LandingPage from './components/LandingPage'
+
 
 function App() {
   return (
     <>
     <div>
-      hi
+      neeche landing page:
     </div>
+    <LandingPage/>
     </>
   )
 }
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,9 @@
+
+export default function LandingPage(){
+
+    return <div>
+        <div>
+            <h1>MESSY PROGRAMMER</h1>
+        </div>
+    </div>
+}
